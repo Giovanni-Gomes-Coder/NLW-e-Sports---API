0759eabe9e92e6e4241e1893e74257c14ec86f16
Giovanni Gomes de Oliveira <giovannioliveira2002@gmail.com>
2022-10-10 15:22:40 -0300

    adjust route /games/:id/ads

--- src/server.ts
+++ src/server.ts
@@ -57,6 +57,7 @@ app.get('/games/:id/ads', async (req, res) => {
       name: true,
       weekDays: true,
       useVoiceChannel: true,
+      yearsPlaying: true,
       hourStart: true,
       hourEnd: true,
     },
